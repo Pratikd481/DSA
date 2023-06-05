4c3af8a18aef8c49849f6f63859eac5aa36ced55
pratik <pratikd481@gmail.com>
2023-06-05 08:47:23 +0530

    C++ pairs

---
+++ STL/pairs.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <utility>
+
+using namespace std;
+
+int main()
+{
+    pair<int, int> p = {1, 2};
+
+    cout << p.first << " " << p.second << endl;
+
+    pair<int, pair<int, int>> nestedPair = {1, {2, 3}};
+    cout << nestedPair.first << " " << nestedPair.second.first << " " << nestedPair.second.second << endl;
+
+    // array of pairs
+    pair<int, int> arr[] = {{1, 2}, {2, 3}};
+    cout << arr[0].first << " " << arr[0].second;
+}
